a0b251393d203dc5919a7c0b56acc76b3b34b566
Versifiction <marc_charpentier@hotmail.fr>
2020-09-09 20:04:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useTheme } from "./ThemeContext";
+import styled, { withTheme } from "styled-components";
+import {
+  backgroundColor,
+  buttonBackgroundColor,
+  buttonTextColor,
+  textColor,
+} from "./theme";
 
-function App() {
+function App(props) {
+  // useEffect(() => {
+  //   if (!localStorage.getItem("theme")) {
+
+  //   }
+  //   localStorage.setItem("theme", props.theme.mode);
+  // }, [props]);
+
+  const themeToggle = useTheme();
+
+  const Header = styled.header`
+    background: ${backgroundColor};
+  `;
+
+  const Text = styled.p`
+    color: ${textColor};
+  `;
+
+  const Button = styled.button`
+    background: ${buttonBackgroundColor};
+    border: none;
+    border-radius: 0.3em;
+    box-shadow: none;
+    color: ${buttonTextColor};
+    cursor: pointer;
+    font-size: 1em;
+    padding: 0.5em 1em;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Test de Dark/Light Mode avec l'API Contexte et les Styled Components
+      </p>
+      <p>
+        <Button onClick={() => themeToggle.toggle()}>
+          {props.theme.mode === "dark"
+            ? "Passer en Mode Light"
+            : "Passer en Mode Dark"}
+        </Button>
+      </p>
+    </header>
   );
 }
 
-export default App;
+export default withTheme(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { MyThemeProvider } from "./ThemeContext";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <MyThemeProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </MyThemeProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
